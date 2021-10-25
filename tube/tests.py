5c1ca0011bbac4d04e11b9fa7ed87bf45cda1e8a
Nishant <kabariyanishant@gmail.com>
2021-10-25 10:27:28 +0530

    added url tag for page redirection in basic.html

--- tube/tests.py
+++ tube/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.test.testcases import SimpleTestCase
 
 # Create your tests here.
+# class Simple(SimpleTestCase):
+#     def test_homepage(self):
+#         response = self.client.get('/')
+#         self.assertEqual(response.status_code, 200)
+    # def test_watchVideo_page(self):
+    #     response = self.client.get('/watch/<int>/')
+    #     self.assertEqual(response.status_code, 200)
